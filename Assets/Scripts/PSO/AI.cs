88b1bd5fad1ff8a88f091c541427e494e89e2d73
EngMahmoudMagdy <engmahmoudmagdy11@gmail.com>
2017-12-02 02:14:51 +0200

    magdy

---
+++ Assets/Scripts/PSO/AI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+	public Transform target,ghost1 ,ghost2,ghost3 ;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
